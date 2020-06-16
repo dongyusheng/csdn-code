54a662a8f51c77f635afca46a590a982a9f10ea7
DongShao <52525198+dongyusheng@users.noreply.github.com>
2020-06-16 12:23:06 +0800

    Update version.c

--- ZeroMQ/version.c
+++ ZeroMQ/version.c
@@ -1,4 +1,6 @@
 //version.c
+//https://blog.csdn.net/qq_41453285/article/details/105990320
+//https://github.com/dongyusheng/csdn-code/edit/master/ZeroMQ/version.c
 #include <stdio.h>
 #include <zmq.h>
 
