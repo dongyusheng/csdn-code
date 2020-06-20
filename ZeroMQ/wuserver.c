6b1393cfaea56bc3ef829651b89574db22cc6965
DongShao <52525198+dongyusheng@users.noreply.github.com>
2020-06-20 20:25:25 +0800

    Update wuserver.c

--- ZeroMQ/wuserver.c
+++ ZeroMQ/wuserver.c
@@ -1,63 +1,73 @@
 //wuserver.c
-//https://blog.csdn.net/qq_41453285/article/details/106005939
+//https://blog.csdn.net/qq_41453285/article/details/106877202
 //https://github.com/dongyusheng/csdn-code/blob/master/ZeroMQ/wuserver.c
 #include <stdio.h>
+#include <stdlib.h>
 #include <assert.h>
 #include <unistd.h>
-#include <stdlib.h>
+#include <string.h>
 #include <time.h>
 #include <zmq.h>
-#include <string.h>
 
-#define randof(num)  (int)((float)(num)*random()/(RAND_MAX + 1.0))
-static int s_send (void *socket, char *string);
+// 随机生成0...num-1的随机数
+#define randof(num)  (int) ((float) (num) * random () / (RAND_MAX + 1.0))
+
+// 将string消息格式化为zmq_meg_t对象, 然后发往socket套接字上
+static int s_send(void *socket, char *string);
 
 int main()
 {
-    //创建新的上下文
-    void* context = zmq_ctx_new();
+    // 1.初始化上下文
+    void *context = zmq_ctx_new();
 
-    //创建PUB套接字
-    void* publisher = zmq_socket(context, ZMQ_PUB);
-    int rc = zmq_bind(publisher, "tcp://*:5556");
+    // 2.创建、绑定套接字
+    void *publisher = zmq_socket(context, ZMQ_PUB);
+    assert(publisher != NULL);
+
+    // 此处我们将发布者绑定到一个tcp节点上和一个ipc节点上, 但是本案例我们只使用tcp, ipc那个只是演示说明zmq的套接字可以绑定到多个节点上
+    int rc = zmq_bind(publisher, "tcp://*:5555");
     assert(rc == 0);
     rc = zmq_bind(publisher, "ipc://weather.ipc");
     assert(rc == 0);
 
-    //初始化随机数发生器
+    // 3.初始化随机数发生器
     srandom((unsigned)time(NULL));
 
-    //循环发送数据
+    // 4.循环发送数据
     while(1)
     {
-        //随机生成邮政编码、温度、适度
+        // 5.随机生成邮政编码、温度、适度
         int zipcode, temperature, relhumidity;
-        zipcode     = randof(100000);
+        zipcode = randof(100000);
         temperature = randof(215) - 80;
         relhumidity = randof(50) + 10;
 
-        //将消息发送给所有的订阅者
+        // 6.将消息发送给所有的订阅者
         char update[20];
         sprintf(update, "%05d %d %d", zipcode, temperature, relhumidity);
-        s_send(publisher, update);
+        rc = s_send(publisher, update);
+        assert(rc);
     }
 
+    // 7.关闭套接字、销毁上下文
     zmq_close(publisher);
-    zmq_ctx_term(context);
+    zmq_ctx_destroy(context);
+
     return 0;
 }
 
-//向指定的socket发送字符串
-static int s_send (void *socket, char *string) 
+static int s_send(void *socket, char *string)
 {
-    int rc;
-    zmq_msg_t message;
-    zmq_msg_init (&message);
-    
-    memcpy (zmq_msg_data (&message), string, strlen (string));
-    rc = zmq_msg_send (&message, socket, 0);
-    assert (!rc);
-    
-    zmq_msg_close (&message);
-    return (rc);
+    // 初始化一个zmq_msg_t对象, 分配的大小为string的大小
+    zmq_msg_t msg;
+    zmq_msg_init_size(&msg, strlen(string));
+    memcpy(zmq_msg_data(&msg), string, strlen(string));
+
+    // 发送数据
+    int rc = zmq_msg_send(&msg, socket, 0);
+
+    // 关闭zmq_msg_t对象
+    zmq_msg_close(&msg);
+
+    return rc;
 }
