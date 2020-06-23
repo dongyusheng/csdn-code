2feeb1b6a5cd9e15c6c4b3ae1b4dea77edbe3146
DongShao <52525198+dongyusheng@users.noreply.github.com>
2020-06-23 14:18:24 +0800

    Update taskwork.c

--- ZeroMQ/taskwork.c
+++ ZeroMQ/taskwork.c
@@ -1,5 +1,5 @@
 //taskwork.c
-//https://blog.csdn.net/qq_41453285/article/details/106009184
+//https://blog.csdn.net/qq_41453285/article/details/106922554
 //https://github.com/dongyusheng/csdn-code/edit/master/ZeroMQ/taskwork.c
 #include <stdio.h>
 #include <stdlib.h>
