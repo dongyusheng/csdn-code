69d1c400225e067414ea39ab7ba91c103f2d259f
DongShao <52525198+dongyusheng@users.noreply.github.com>
2020-06-24 15:04:56 +0800

    Update tasksink.c

--- ZeroMQ/tasksink.c
+++ ZeroMQ/tasksink.c
@@ -6,64 +6,73 @@
 #include <string.h>
 #include <unistd.h>
 #include <time.h>
-#include <zmq.h>
 #include <sys/time.h>
+#include <zmq.h>
 
-static char* s_recv(void* sokcet);
-//返回当前系统时钟,以毫秒返回
+// 从socket中接收数据
+static char* s_recv(void* socket);
+
+// 返回当前系统时钟,以毫秒返回
 static int64_t s_clock (void);
 
 int main()
 {
-    //创建新的上下文
+    // 1.创建新的上下文
     void *context = zmq_ctx_new();
 
-    //创建套接字,用于接收从
+    // 2.创建、绑定套接字
+    //   发生器、工人都会连接这个套接字, 并向这个套接字发送数据
     void *receiver = zmq_socket(context, ZMQ_PULL);
     zmq_bind(receiver, "tcp://*:5558");
 
-    //等待批次的开始
+    // 3.这是接收的第一条消息, 从发生器那里接收的, 是发生器用来告诉当前接收器, 表示开始工作了
     char *string = s_recv(receiver);
     free(string);
 
-    //启动时钟
+    // 4.启动时钟
     int64_t start_time = s_clock();
 
-    //处理100个确认
+    // 5.从工人那里接收100个确认, 因为发生器只分配了100个任务给工人
     int task_nbr;
     for(task_nbr = 0; task_nbr < 100; task_nbr++)
     {
+        // 6.接收数据
         char *string = s_recv(receiver);
         free(string);
+
+        // 7.每接收10个任务打印一次:号, 其余打印.
         if((task_nbr / 10) * 10 == task_nbr)
             printf(":");
         else
             printf(".");
         fflush(stdout);
     }
-    //打印一下总共执行了多长时间
+    
+    // 8.处理完成之后打印一下总共执行了多长时间, 也就是工人一共工作了多长时间
     printf("Total elapsed time: %d msec\n",(int)(s_clock() - start_time));
 
-    //善后处理
+    // 9.关闭套接字、销毁上下文
     zmq_close(receiver);
-    zmq_ctx_term(context);
+    zmq_ctx_destroy(context);
+    
     return 0;
 }
 
 static char* s_recv(void* socket)
 {
-    zmq_msg_t message;
-    zmq_msg_init(&message);
+    zmq_msg_t msg;
+    zmq_msg_init(&msg);
  
-    int size = zmq_msg_recv(&message, socket, 0);
-    if(size == -1)
+    int rc = zmq_msg_recv(&msg, socket, 0);
+    if(rc == -1)
         return NULL;
  
-    char *string = (char*)malloc(size + 1);
-    memcpy(string, zmq_msg_data(&message), size);
+    char *string = (char*)malloc(rc + 1);
+    memcpy(string, zmq_msg_data(&msg), rc);
  
-    zmq_msg_close(&message);
-    string[size] = 0;
+    zmq_msg_close(&msg);
+    string[rc] = 0;
+    
     return string;
 }
 
