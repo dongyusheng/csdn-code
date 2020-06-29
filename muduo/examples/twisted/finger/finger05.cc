22201e6d0adffd875ff75ab04552c24b95b244a9
DongShao <52525198+dongyusheng@users.noreply.github.com>
2020-06-29 11:23:21 +0800

    Create finger05.cc

---
+++ muduo/examples/twisted/finger/finger05.cc
@@ -0,0 +1,27 @@
+// finger05.cc
+// https://blog.csdn.net/qq_41453285/article/details/107015436
+#include "muduo/net/EventLoop.h"
+#include "muduo/net/TcpServer.h"
+
+using namespace muduo;
+using namespace muduo::net;
+
+void onMessage(const TcpConnectionPtr& conn,
+               Buffer* buf,
+               Timestamp receiveTime)
+{
+    if (buf->findCRLF())
+    {
+        conn->send("No such user\r\n");
+        conn->shutdown();
+    }
+}
+
+int main()
+{
+    EventLoop loop;
+    TcpServer server(&loop, InetAddress(1079), "Finger");
+    server.setMessageCallback(onMessage);
+    server.start();
+    loop.loop();
+}
